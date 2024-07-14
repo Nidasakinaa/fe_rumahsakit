64f17549c85eeeed54f6c7c66ff3e8b7b3251c56
Nidasakinaa <nidasakinaaulia@gmail.com>
2024-07-14 09:56:50 +0700

    bismillah ya allahh

--- js/fetch.js
+++ js/fetch.js
@@ -2,6 +2,7 @@ import { get } from "https://bukulapak.github.io/api/process.js";
 import { isiTablePasien } from "./controller/get";
 let urlAPI = "https://ws-rumahsakit-2e1eb71f14e2.herokuapp.com/pasien";
 get(urlAPI,isiTablePasien);
-function isiTablePasien(results){
-    console.log(results);
-}
+
+// function isiTablePasien(results){
+//     console.log(results);
+// }

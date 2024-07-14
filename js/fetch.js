7a4ce9d77d2ddf10809975e6ea73e009c3e3cf72
Nidasakinaa <nidasakinaaulia@gmail.com>
2024-07-14 10:08:08 +0700

    bismillah

--- js/fetch.js
+++ js/fetch.js
@@ -3,6 +3,3 @@ import { isiTablePasien } from "./controller/get";
 let urlAPI = "https://ws-rumahsakit-2e1eb71f14e2.herokuapp.com/pasien";
 get(urlAPI,isiTablePasien);
 
-// function isiTablePasien(results){
-//     console.log(results);
-// }

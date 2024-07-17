95e3c6db92e5a590280453d9e6cd7d5a71387ff5
Nidasakinaa <nidasakinaaulia@gmail.com>
2024-07-17 20:53:22 +0700

    FINAL PUSHH YA ALLAH ALHAMDULILLAHH

--- js/temp/table.js
+++ js/temp/table.js
@@ -1,13 +1,7 @@
 export let isiTabel = 
 `
 <tr class="h-18 border-b border-coolGray-100">
-    <th class="whitespace-nowrap px-4 bg-white text-left">
-        <div class="flex items-center -m-2">
-            <div class="w-auto p-2">
-                <p class="text-xs font-semibold text-coolGray-800">#NAMA#</p>
-            </div>
-        </div>
-    </th>
+    <th class="whitespace-nowrap px-4 bg-white text-sm font-medium text-coolGray-800 text-left">#NAMA#</th>
     <th class="whitespace-nowrap px-4 bg-white text-sm font-medium text-coolGray-800 text-left">#GENDER#</th>
     <th class="whitespace-nowrap px-4 bg-white text-sm font-medium text-coolGray-800 text-center">#USIA#</th>
     <th class="whitespace-nowrap px-4 bg-white text-sm font-medium text-coolGray-500 text-left">#NOHP#</th>

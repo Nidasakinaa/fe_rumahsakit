6d52aff87967c686ffdb98a1f6b6843fa1b71650
Nidasakinaa <nidasakinaaulia@gmail.com>
2024-07-19 14:02:38 +0700

    bismillah push

--- js/config/url_post.js
+++ js/config/url_post.js
@@ -3,5 +3,5 @@ export let urlPOST = "https://ws-rumahsakit-2e1eb71f14e2.herokuapp.com/insert"
 export function AmbilResponse(result) {
     console.log(result); 
     alert(result.message); 
-    window.location.reload(); 
+    window.location.href="index.html"; 
 }

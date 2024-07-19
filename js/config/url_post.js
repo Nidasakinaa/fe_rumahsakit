d4a399cae3f6b598877a43d74091a3fecf00bb7f
Nidasakinaa <nidasakinaaulia@gmail.com>
2024-07-19 14:23:46 +0700

    a

--- js/config/url_post.js
+++ js/config/url_post.js
@@ -2,6 +2,6 @@ export let urlPOST = "https://ws-rumahsakit-2e1eb71f14e2.herokuapp.com/insert"
 
 export function AmbilResponse(result) {
     console.log(result); 
-    alert(result.message); 
-    window.location.href="index.html"; 
+    // alert(result.message); 
+    window.location.href = "index.html"; 
 }
